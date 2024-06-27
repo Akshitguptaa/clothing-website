a8fe5448ba1b6c1e9c2a94c06d3ec339f255c255
Akshit Gupta <akshitguptaa29@gmail.com>
2024-06-28 01:59:51 +0530

    minor changes

--- src/components/directory-item/directory-item.components.jsx
+++ src/components/directory-item/directory-item.components.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 import { DirectoryItemContainer, BgImg, DirectoryBody } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title ,route} = category;
+
+    const navigate= useNavigate();
+
+    const onNavigateHandler= ()=> navigate(route);
+
     return (
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={onNavigateHandler}>
             <BgImg style={{ backgroundImage: `url(${imageUrl})`}} />
             <DirectoryBody>
                 <h2>{title}</h2>
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,36 +1,8 @@
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://images.unsplash.com/photo-1556306535-0f09a537f0a3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://images.unsplash.com/photo-1493568000180-ca2fb70ddcba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://cdn.pixabay.com/photo/2016/11/19/18/06/feet-1840619_1280.jpg",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://images.unsplash.com/photo-1590649880765-91b1956b8276?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://images.unsplash.com/photo-1504593811423-6dd665756598?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ];
-
   return (
-    <Directory categories={categories}/>
+    <Directory/>
   );
 };
 
